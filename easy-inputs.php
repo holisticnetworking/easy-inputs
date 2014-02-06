151e5d9e11600995c751ebc39cd93a37b7a3fe9f
Tom Belknap <tbelknap@holisticnetworking.net>
2014-02-06 13:30:03 -0500

    Lots of reorganization and the beginnings of the inputs themselves. Have not gotten to saving, but need to make sure I'm clear on how the basics of the inputs will work.

--- easy-inputs.php
+++ easy-inputs.php
@@ -29,65 +29,194 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 */
 
 class EasyInputs {
-	var $name	= 'EasyInputs';
-	/*///////////////
-	//  The Forms  //
-	///////////////*/
-	public function input( $args ) {
+	var $name		= 'EasyInputs';
+	var $validate	= 'EasyInputs::validate()';
+	
+	
+	/*
+	 * nonce:			Don't overthink it. Just let WordPress handle creating the nonce.
+	 * 					This function returns, rather than outputs, the nonce, in case we
+	 * 					need to do something further before output.
+	 * @var str $name:	The name we wish to call the nonce by
+	 */
+	public function nonce( $action=null, $name=null ) {
+		if( !($action) ) $action = plugin_basename( __FILE__ );
+		if( !($name) ) $name = $this->name;
+		return wp_nonce_field( $action, $name, true, false );
+	}
+	
+	
+	/*
+	 * group:			Defines a group of inputs, both logically and physically.
+	 * 					Logically, this group is associated with a single nonce to
+	 * 					which it is bound. Physically, all elements of a group will
+	 * 					be displayed together, in a fieldset, if requested.
+	 * @var arr $args:	Our array of arguments, see below.
+	 * $args	= array(
+	 * 		'fieldset'	=> array(
+	 * 							'legend'	=> null,
+	 * 							'disabled' 	=> false,
+	 * 							'name'		=> null
+	 * 						),
+	 * 		'inputs'	=> array()
+	 * )
+	 */
+	public function group( $name=null, $args=array() ) {
+		if( !$name or empty( $args ) ) return;
 		extract( $args );
-		$name		= isset($name) ? esc_attr($name) : 'blank';
-		$label		= isset($label) ? esc_attr($name) : null;
-		$value		= isset($value) ? esc_attr($name) : '';
-		$size		= isset($size) ? esc_attr($size) : 20;
-		$maxlength	= isset($maxlength) ? esc_attr($maxlength) : 100;
-		$wrap		= isset($wrap) ? $wrap : true;
-		$class		= isset($class) ? 'class="' . $class . '"' : null;
+		if( empty( $action ) ) $action = plugin_basename( __FILE__ );
 		
-		$input		= '';
-		if( $wrap ) : $input .= '<p>'; endif;
-		if(!empty($label)) :
-			echo '<label for="' . $name . '">' . $label . ':</label>';
+		// Each group gets its own nonce automatically:
+		$result	= $this->nonce( $action, $name . '_nonce' );
+		
+		// Append our fieldset, if required:
+		$result	.= !empty( $fieldset ) ? $this->fieldset_open( $fieldset ) : '';
+		// Append each input per it's own function, else the generic input function:
+		foreach( $inputs as $input ) :
+			if( is_array( $input ) ) :
+				if( method_exists( 'EasyInputs', $input['type'] ) ) :
+					$result	.= $this->$input['type']( $input );
+				else :
+					$result .= $this->input( $input );
+				endif;
+			else :
+				$result	.= $this->input( $input, null, $name );
+			endif;
+		endforeach;
+		// Close the fieldset:
+		$result	.= !empty( $fieldset ) ? $this->fieldset_close() : '';
+		return $result;
+	}
+	
+	
+	/*
+	 * fieldset_open/close:		Creates a fieldset with optional legend
+	 * @var arr $args:			'attrs' array and optional legend info
+	 */
+	public function fieldset_open( $args ) {
+		extract( $args );
+		$attr	= empty( $attr ) ? '' : $this->attrs_to_str( $attrs );
+		$legend	= empty( $legend ) ? '' : $this->legend( $legend );
+		return sprintf( '<fieldset %s>%s', $attr, $legend );
+	}
+	public function fieldset_close() {
+		return '</fieldset>';
+	}
+	
+	
+	/*
+	 * legend:			Outputs an HTML legend
+	 * @var arr @args:	A title and optional 'attr' list
+	 */
+	public function legend( $args ) {
+		extract( $args );
+		if( empty( $title ) ) :
+			return null;
 		endif;
-		echo '<input type="text" ' . $class . ' id="' . $name . '" name="easy-inputs[' . $name . ']" value="' . esc_attr($value) . '" size="' . $name . '" maxlength="' . $name . '" />';
-		if( $wrap ) : $input .= '</p>'; endif;
-		echo $input;
-	}
-	
-	// One nonce for all Easy Inputs data:
-	public function nonce() {
-		wp_nonce_field( 'easy-inputs', $this->name );
+		$attr	= empty( $attr ) ? '' : $this->attrs_to_str( $attrs );
+		return sprintf( '<legend %s>%s</legend>', $attr, $title );
 	}
 	
 	
-	// Check for and save Easy Inputs fields:
-	public function save_inputs( $post_id ) {
-		if(!empty($_POST['easy-inputs'])) :
-			// Refuse without valid nonce:
-			if ( ! isset( $_POST[$this->name . '_nonce'] ) || ! wp_verify_nonce( $_POST[$this->name . '_nonce'], 'easy-inputs' ) ) return;
-			foreach($_POST['easy-inputs'] as $key=>$input) :
-				
-				
-				// Sanitize input:
-				$value	= EasyInputs::sanitize($input['value']);
-			endforeach;
+	/*
+	 * THE FIELDS
+	 */
+	
+	/*
+	 * input:			The default and also the model for all inputs structure
+	 * @var str $field:	The field name.
+	 * @var str $args:	A collection of additional arguments, formatted below
+	 * $args	= array(
+	 * 		$attrs	= arr, <-- HTML attributes
+	 * 		$value	= str, <-- The value of the field, defaults to blank
+	 * 		$type	= str, <-- The HTML Field type (text, checkbox, etc). 
+	 * 						   Defaults to 'text'
+	 * 		$name	= str, <-- The fully-qualified name attribute, if desired.
+	 * 		$group	= str  <-- The group to which this element belongs. 
+	 * );
+	 */
+	public function input( $field=null, $args=array(), $group=null ) {
+		if( !$field ) return;
+		if( is_array( $args ) ) extract( $args );
+		$group	= !empty( $group ) ? $group : null;
+		$attr	= !empty( $attrs ) ? $this->attrs_to_str( $attrs ) : '';
+		$value	= !empty( $value ) ? $value : '';
+		$type	= !empty( $type ) ? $type : 'text';
+		$name	= !empty( $name ) ? $name : $this->field_name( $field, $group );
+		
+		// Handle creating a label:
+		$html_label	= '';
+		if( !isset( $label ) ) :
+			$html_label = $this->label( $field, $field );
+		elseif( is_string( $label ) ) :
+			$html_label = $this->label( $label, $field );
 		endif;
+		
+		return sprintf(
+			'%s<input id="%s" type="%s" name="%s" %s value="%s" />',
+			$html_label,
+			$field,
+			$type,
+			$name,
+			$attr,
+			$value
+		);
 	}
 	
-	private function sanitize($val) {
-		if(is_array($val)) :
-			foreach($val as $k=>$v) :
-				$val[$k]	= EasyInputs::sanitize($v);
-			endforeach;
-		else :
-			$val	= sanitize_text_field($val);
-		endif;
-		return $val;
+	
+	/*
+	 * label:			Create an HTML label
+	 */
+	public function label( $str=null, $for=null, $attrs=null ) {
+		$str	= ucwords( implode( ' ', explode( '_', $str ) ) );
+		$is_for	= !empty( $for ) ? sprintf( 'for="%s"', $for ) : '';
+		$attr	= is_array( $attrs ) ? $this->attrs_to_str( $attrs ) : '';
+		return sprintf( '<label %s %s>%s</label>', $is_for, $attr, $str );
 	}
+	
+	
+	
+	/*
+	 * Utility functions
+	 */
+	
+	/*
+	 * attrs_to_str:		Convert HTML attributes
+	 * 						This could stand some security features, but for
+	 * 						now, it's barebones. I don't want to get in the
+	 * 						way of HTML5 attributes and data attributes by
+	 * 						over-thinking security at this stage.
+	 */
+	public function attrs_to_str( $attrs=null ) {
+		if( !is_array( $attrs ) ) return;
+		$to_string	= array();
+		foreach( $attrs as $key=>$val ) :
+			$to_string[]	= sprintf( '%s="%s"', $key, htmlspecialchars( $val ) );
+		endforeach;
+		return implode( ' ', $to_string );
+	}
+	
+	/*
+	 * field_name:			Assigns a valid field name for the given input args
+	 * @var str $field:		The field-specific name.
+	 */
+	public function field_name( $field=null, $group=null ) {
+		if( !$field ) return;
+		$group	= !empty( $group ) ? sprintf( '[%s]', $group ) : '';
+		return sprintf( '%s%s[%s]', $this->name, $group, $field);
+	}
+	
+	
+	
+	
+	
+	
+	
 	
 	// Ready, steady, go:
 	public function __construct( $name='EasyInputs' ) {
 		$this->name	= $name;
 		// Check for Easy Inputs on save:
-		add_action( 'save_post', 'EasyInputs::save_inputs' );
+		add_action( 'save_post', 'EasyInputs::save' );
 	}
 }
